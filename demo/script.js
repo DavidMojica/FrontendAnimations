270704d2b22f403c5845ca389d62bef667203c8e
David Mojica <davidmojicav@gmail.com>
2023-09-27 14:10:14 -0500

    upload 5

--- demo/script.js
+++ demo/script.js
@@ -1,100 +1,120 @@
 //Css
-const cssBg                   = document.getElementById("background_animation")
+const cssBg = document.getElementById("background_animation")
 //script
-const src_cursor_follower     = document.getElementById('src_cursor_follower');
+const src_cursor_follower = document.getElementById('src_cursor_follower');
 
-const middle_section          = document.getElementById('middle_section');
-const btn_card_magic_1        = document.getElementById('card_magic_1');
+const middle_section = document.getElementById('middle_section');
+const btn_card_magic_1 = document.getElementById('card_magic_1');
+const secret_bg_1 = document.getElementById('secret_bg_1');
+const disable_sbg1 = document.getElementById('disable_sbg1');
 const btn_loading_animation_1 = document.getElementById('btn_loading_animation_1');
-const btn_a_press_animation   = document.getElementById('btn_a_press_animation');
-const btn_bg_fire_bubbles     = document.getElementById('btn_bg_fire_bubbles');
-const cf_magic_trace          = document.getElementById('cf_magic_trace');
+const btn_a_press_animation = document.getElementById('btn_a_press_animation');
+const btn_bg_fire_bubbles = document.getElementById('btn_bg_fire_bubbles');
+const cf_magic_trace = document.getElementById('cf_magic_trace');
+const blind_footer = document.getElementById('i-blind_footer');
+const hiding_footer = document.getElementById('hiding_footer');
+/*ICONS*/
+blind_footer.addEventListener('click', function () {
+    hiding_footer.style.opacity = hiding_footer.style.opacity === '0' ? '1' : '0';
+    blind_footer.classList.toggle('fa-eye-slash');
+    blind_footer.classList.toggle('fa-eye');
+});
 
 /*BACKGROUNDS*/
-btn_bg_fire_bubbles.addEventListener('click', function(){
+btn_bg_fire_bubbles.addEventListener('click', () => {
     cssBg.setAttribute('href', "bg_fire_bubbles/style.css")
 });
 
-/*CARDS*/
-btn_card_magic_1.addEventListener('click', function(){
-    if(document.getElementsByClassName('cards').length == 0){
-        const card = document.createElement('div');
-        card.setAttribute('class','card_magic_1 cards');
-        middle_section.appendChild(card);
+secret_bg_1.addEventListener('click', () => {
+    const card = createMagicCard_1() || document.getElementById('magic_card_1');
+    (card || document.getElementById('magic_card_1')).style.display = "contents";
+});
 
-        //interacción con loading
-        const things = document.querySelectorAll('.things');
-        if(things.length >= 1)
-            for(let thing of things)
-                card.appendChild(thing);
+disable_sbg1.addEventListener('click', () => {
+    if (document.getElementById('magic_card_1').style.display == "contents") {
+        document.getElementById('magic_card_1').style.display = "grid";
     }
 });
 
-function deleteCard(){
-    const card = document.querySelector('.cards');
-    if(card) card.remove();
+/*CARDS*/
+btn_card_magic_1.addEventListener('click', createMagicCard_1);
+
+function createMagicCard_1() {
+    const cards = document.getElementsByClassName('cards');
+
+    if (cards.length === 0) {
+        const card = document.createElement('div');
+        card.classList.add('card_magic_1', 'cards');
+        card.id = 'magic_card_1';
+        middle_section.appendChild(card);
+
+        const things = document.querySelectorAll('.things');
+        things.forEach(thing => card.appendChild(thing));
+
+        return card;
+    } else {
+        const magicCard = document.getElementById('magic_card_1');
+        if (magicCard) magicCard.style.display = 'grid';
+
+        return false;
+    }
 }
 
-function verificarCarta(){
+function deleteCard() {
     const card = document.querySelector('.cards');
-    if(card) return card;
-    else return false;
+    card?.remove();
+}
+
+function verificarCarta() {
+    const card = document.querySelector('.cards');
+    return card || false;
 }
 
 /*LOADS*/
-btn_loading_animation_1.addEventListener('click', function(){
-    if(document.getElementsByClassName('loadings').length == 0){
+btn_loading_animation_1.addEventListener('click', () => {
+    const loadings = document.getElementsByClassName('loadings');
+
+    if (loadings.length === 0) {
         const h1 = document.createElement('h1');
-        h1.setAttribute('data-text', "Loading...");
-        h1.setAttribute('class', 'loading_animation loadings things')
-        h1.textContent = "Loading...";
-        
-        //Interacción con card
-        if(verificarCarta()){
-            card = verificarCarta();
-            card.appendChild(h1);
-        } else middle_section.appendChild(h1);
+        h1.setAttribute('data-text', 'Loading...');
+        h1.classList.add('loading_animation', 'loadings', 'things');
+        h1.textContent = 'Loading...';
+
+        const targetNode = verificarCarta() || middle_section;
+        targetNode.appendChild(h1);
     }
 });
 
-function deleteLoading(){
+function deleteLoading() {
     const loading = document.querySelector('.loadings');
-    if(loading) loading.remove();
+    loading?.remove();
 }
 
+/*FORMS - BUTTONS*/
 
+btn_a_press_animation.addEventListener('click', () => {
+    const buttons = document.getElementsByClassName('buttons');
 
-/*FORMS*/
-/*BUTTONS*/
-
-btn_a_press_animation.addEventListener('click', function(){
-    if(document.getElementsByClassName('buttons').length == 0){
+    if (buttons.length === 0) {
         const button = document.createElement('button');
-        button.setAttribute('class', 'btn_press_animation buttons things');
-        button.textContent = "Golpeame";
+        button.classList.add('btn_press_animation', 'buttons', 'things');
+        button.textContent = 'Golpeame';
 
-        //Interacción con card
-        if(verificarCarta()){
-            card = verificarCarta();
-            card.appendChild(button);
-        } else middle_section.appendChild(button);
+        const targetNode = verificarCarta() || middle_section;
+        targetNode.appendChild(button);
     }
 });
 
-function deleteButtons(){
+function deleteButtons() {
     const button = document.querySelector('.buttons');
-    if(button) button.remove();
+    button?.remove();
 }
 
-
-function CssCambiarRuta(archivo, ruta){
-    const a = document.getElementById(archivo);
-    a.setAttribute("href", ruta)
+function CssCambiarRuta(archivo, ruta) {
+    document.getElementById(archivo)?.setAttribute('href', ruta);
 }
 
-function JScambiarRuta(archivo, ruta){
+function JScambiarRuta(archivo, ruta) {
     const scriptElement = document.getElementById(archivo);
-    scriptElement.setAttribute('src', ruta)
-    scriptElement.src = ruta;
+    if (scriptElement) scriptElement.src = ruta;
 }
-//onclick="JScambiarRuta('src_cursor_follower', 'cursorFollower_magic_particles/script.js')"
